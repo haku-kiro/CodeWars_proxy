aedc3079ffa42f0170029f6b4e8de41952187a88
haku-kiro <botprime@yahoo.com>
2019-04-21 00:37:18 +0200

    Adding, testing axios - testing codewars endpoint

--- express/server.js
+++ express/server.js
@@ -4,6 +4,7 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const router = express.Router();
 router.get('/', cors(), (req, res) => {
@@ -15,6 +16,19 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 router.get('/thing', cors(), (req, res, next) => {
   res.send({ name: 'Michael', surname: 'Da Costa' });
+});
+
+router.get('/codewars', cors(), (req, res, next) => {
+  var that = res;
+
+  axios
+    .get('https://www.codewars.com/api/v1/users/haku_kiro')
+    .then(response => {
+      that.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.use(bodyParser.json());
